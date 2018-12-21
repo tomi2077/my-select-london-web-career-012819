d8f2aff879a048196895f6b6ebaabde39b5cbc19
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-21 15:33:13 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  i = 0
+  new_collecton = []
+  while i < array.length
+    if yield(array[i]) == true
+      new_collecton << array[i]
+    else
+    end
+    i += 1
+  end
+  new_collecton
 end
+
+  
