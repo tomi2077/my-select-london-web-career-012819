dc12142bac194ee39f2d47a0c42f0fb04da01d09
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-21 15:22:47 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,23 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  select_items = []
+  while i < collection.length
+  select_items << yield { collection[|i| i }
+  end
+  select_items
+end
+
+
+def my_select(array)
+  i = 0
+  new_array = []
+  while i < array.size
+    if yield(array[i]) == true
+      new_array << array[i]
+    else
+      #nothing
+    end
+    i += 1
+  end
+  new_array
 end
